0f9dfc87d527717da2c89767d3933032cfb7e755
arai-yui <arai-yui@rikkyo.ac.jp>
2022-01-14 17:38:00 +0900

    MNAL-195 パンくず表示を追加する

--- cms/wp-content/themes/wikiwp/404.php
+++ cms/wp-content/themes/wikiwp/404.php
@@ -4,24 +4,19 @@ get_template_part('navigation');
 ?>
 
 <div class="pageContainer">
-	<article class="entry entryTypePost">
-		<header class="entryHeader">
-			<h1 class="entryTitle">
-				<?php echo '404 - '. __('Page not found', 'wikiwp'); ?>
-			</h1>
-		</header>
-	</article>
-
-
-	<div class="last-posts-list postinfo clearfix">
-		<hr>
-		<h4 class="lastPostsListTitle">
-			<?php echo __('Last posts', 'wikiwp'); ?>
-		</h4>
-
-		<ul>
-			<?php wp_get_archives('type=postbypost&limit=10'); ?>
-		</ul>
+	<div class="pageInner">
+		<div class="entry entryTypePost">
+			<div class="entryHeader">
+				<h1 class="entryTitle">
+					<?php echo '404 - '. __('Page not found', 'wikiwp'); ?>
+				</h1>
+				<div class="entryContent">
+					<p>お探しのページは存在しません。<br>
+					URLの入力ミスがないかを確認するか、トップページへ移動してください。</p>
+					<p class="txt-link"><a href="/">トップページへ戻る</a></p>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
