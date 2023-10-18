982a16a53754076edc62e3b524b65b86de49bc4f
arai-yui <arai-yui@rikkyo.ac.jp>
2023-10-18 17:19:06 +0900

    MNAL-751 iPhone（Safari）検索ボックスの表示調整｜デザイン変更

--- cms/wp-content/themes/wikiwp/searchform.php
+++ cms/wp-content/themes/wikiwp/searchform.php
@@ -2,7 +2,7 @@
 	echo '<form role="search" method="get" class="search-form" action="'.home_url( '/' ).'" autocomplete="off">',
 		 '<label>',
 
-		 '<input type="search" class="search-field" value="'.get_search_query().'" name="s" id="s" title="'.__( 'Search', 'wikiwp' ).'" />',
+		 '<input type="search" class="search-field"  placeholder="Search" value="'.get_search_query().'" name="s" id="s" title="'.__( 'Search', 'wikiwp' ).'" />',
 		 '<input type="submit" class="search-submit" value="" />',
 		 '</label>',
 		 '</form>';
