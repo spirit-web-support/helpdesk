d1aa240ea294f0d1a04ddf3926b8e940f82e0c02
arai-yui <arai-yui@rikkyo.ac.jp>
2023-11-09 18:13:52 +0900

    MNAL-758 新サーバー移行に伴う修正と調査・検証：新テーマを使用する場合｜navigation

--- cms/wp-content/themes/twentytwentytwo-child/js/custom.js
+++ cms/wp-content/themes/twentytwentytwo-child/js/custom.js
@@ -1,24 +1,64 @@
 jQuery(document).ready(function ($) {
-    //ハンバーガーメニュー
+    var lytNav = document.getElementById('lyt-nav');
+
+   //PCメニュー
+   if (lytNav) {
+       var subMenuTitle = document.querySelectorAll('input[id^="sub-menu"]');
+       subMenuTitle.forEach((element) => {
+           element.addEventListener('keydown', (event) => {
+               if (event.key === 'Enter') {
+                   event.target.checked = !event.target.checked;
+               }
+           })
+       })
+   } 
+
+    //SPハンバーガーメニュー
     var btnNav = document.getElementById('btn-nav');
+    var body = document.getElementsByTagName('body');
+
     if (btnNav) {
-        btnNav.addEventListener("keydown", (event) => {
-            if (event.key === "Enter") {
+        function bodyfixed() {
+            body[0].classList.toggle('fixed');
+        }
+
+        btnNav.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
                 btnNav.checked = !btnNav.checked;
-                var body = document.getElementsByTagName('body');
-                body[0].classList.toggle('fixed');
+                bodyfixed();
             }
         });
-        btnNav.addEventListener('change', function () {
-            var body = document.getElementsByTagName('body');
-            body[0].classList.toggle('fixed');
-        }, false);
-    } 
 
-    //フッター日付
-    document.getElementById("current-year").innerText = new Date().getFullYear();
+        btnNav.addEventListener('change', function () {
+            bodyfixed();
+        }, false);
+
+        window.addEventListener('resize', function () {
+            btnNav.checked = false;
+            body[0].classList.remove('fixed');
+        }, false);
+    }
+
+    //SPメニュー追随
+    $(function () {
+        if (lytNav) {
+            var windowWidth = window.innerWidth;
+            if (windowWidth <= 990) {
+                $(window).on('load scroll', function () {
+                    if ($(this).scrollTop() > 200 && lytNav.classList.contains('fixed') == false) {
+                        lytNav.classList.add('fixed');
+                        lytNav.animate({ top: ['-100px', '0'], }, 600);
+                    }
+                    else if ($(this).scrollTop() < 199 && lytNav.classList.contains('fixed') == true) {
+                        lytNav.classList.remove('fixed');
+                    }
+                });
+            }
+        }
+    });
+
     //Page Top
-    $(function() {
+    $(function () {
         $("#page-top").hide();
         $("#page-top a").removeAttr('href');
         $(window).on('load scroll', function () {
@@ -27,25 +67,25 @@ jQuery(document).ready(function ($) {
             var footHeight = $("footer").innerHeight();
             var TopBtnHeight = $("#page-top").innerHeight();
             var windowWidth = window.innerWidth;
-            if(windowWidth <= 990){
+            if (windowWidth <= 990) {
                 NavHeight = $(".navMenu.fixed").innerHeight();
-                if (scrollHeight - scrollPosition  - NavHeight  - TopBtnHeight <= footHeight) {
+                if (scrollHeight - scrollPosition - NavHeight - TopBtnHeight <= footHeight) {
                     $("#page-top").fadeIn(600);
-                    $("#page-top").css({ position: "absolute", bottom: footHeight});
-                } else if($(this).scrollTop() > 100){
+                    $("#page-top").css({ position: "absolute", bottom: footHeight });
+                } else if ($(this).scrollTop() > 100) {
                     $("#page-top").fadeIn(600);
                     $("#page-top").css({ position: "fixed", bottom: "0" });
-                } else if($(this).scrollTop() < 99 ){
+                } else if ($(this).scrollTop() < 99) {
                     $("#page-top").fadeOut(600);
                 }
-            }else{
+            } else {
                 if (scrollHeight - scrollPosition - TopBtnHeight <= footHeight) {
                     $("#page-top").fadeIn(600);
-                    $("#page-top").css({ position: "absolute", bottom: footHeight});
-                } else if($(this).scrollTop() > 100){
+                    $("#page-top").css({ position: "absolute", bottom: footHeight });
+                } else if ($(this).scrollTop() > 100) {
                     $("#page-top").fadeIn(600);
                     $("#page-top").css({ position: "fixed", bottom: "0" });
-                } else if($(this).scrollTop() < 99 ){
+                } else if ($(this).scrollTop() < 99) {
                     $("#page-top").fadeOut(600);
                 }
             }
@@ -54,8 +94,8 @@ jQuery(document).ready(function ($) {
             $('body, html').animate({ scrollTop: 0 }, 500);
             return false;
         });
-        $("#page-top a").on('keydown', function(event){
-            if (event.key === "Enter"){
+        $("#page-top a").on('keydown', function (event) {
+            if (event.key === "Enter") {
                 $('body, html').animate({ scrollTop: 0 }, 500);
                 return false;
             }
