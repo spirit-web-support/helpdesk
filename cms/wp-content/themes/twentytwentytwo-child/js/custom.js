a2eee2eb0f94a417522463e2e1858e027058cf84
arai-yui <arai-yui@rikkyo.ac.jp>
2023-12-13 10:05:07 +0900

    MNAL-776 新サーバー移行に伴う修正と調査・検証：新テーマを使用する場合｜アコーディオンjs｜リンク遷移js

--- cms/wp-content/themes/twentytwentytwo-child/js/custom.js
+++ cms/wp-content/themes/twentytwentytwo-child/js/custom.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function ($) {
+    //グローバルメニュー
     (function () {
         'use strict';
         var lytNav = document.getElementById('lyt-nav');
@@ -106,4 +107,177 @@ jQuery(document).ready(function ($) {
             }
         });
     }());
+
+    //ページ内リンクのスクロール調整
+    $(window).on('load', function () {
+        var url = $(location).attr('href');
+        if (url.indexOf("#") != -1) {
+            var anchor = url.split("#");
+            var target = $('#' + anchor[anchor.length - 1]);
+            if (target.length) {
+                var position = Math.floor(target.offset().top) - 60;
+                $("html, body").scrollTop(position);
+            }
+        }
+    });
+    $('a[href^="#"]:not(.btn-tab):not(#page-top a)').click(function () {
+        var href = $(this).attr("href");
+        var target = $(href);
+        if (target.length) {
+            var position = target.offset().top - 60;
+            $('body,html').stop().animate({ scrollTop: position }, 500);
+        }
+        return false;
+    });
+
+    //アコーディオンモジュール
+    if ($(".acd-01 .acd-panel .acd-panel-heading").length) {
+        $(".acd-01 .acd-panel").addClass("is-closed").attr({ "aria-expanded": "false", "aria-hidden": "true" });
+        $(".acd-01 .acd-panel .acd-panel-heading").attr({ "role": "button", "tabindex": "0"});
+
+        (function () {
+            var all = $('.acd-01 .acd-panel');
+            var i = 0;
+            var max = all.length;
+
+            for (i; i < max; i++) {
+                var headingHeight = ($(all[i])).find('.acd-panel-heading').outerHeight(true);
+                ($(all[i])).height(headingHeight);
+            };
+        }());
+
+        $(".acd-01 .acd-panel .acd-panel-heading").on("click", function () {
+            var tgl = $(this).parents(".acd-panel");
+ 
+            if (tgl.hasClass("is-open") || tgl.hasClass("is-opened")) {
+                tgl.removeClass("is-open").removeClass("is-opened");
+                tgl.addClass("is-close").attr({ "aria-expanded": "false", "aria-hidden": "true" });
+                headingHeight = tgl.find('.acd-panel-heading').outerHeight(true);
+                tgl.height(headingHeight);
+            } else {
+                tgl.removeClass("is-close").removeClass("is-closed");
+                tgl.addClass("is-open").attr({ "aria-expanded": "true", "aria-hidden": "false" });
+                headingHeight = tgl.find('.acd-panel-heading').outerHeight(true);
+                contentHeight = tgl.find('.acd-panel-content').outerHeight(true);
+                tgl.height(headingHeight + contentHeight + "px");
+            }
+            return false;
+        });
+
+        $(".acd-01 .acd-panel .acd-panel-heading").on('keydown', function (event) {
+            if (event.key === "Enter") {
+                var tgl = $(this).parents(".acd-panel");
+
+                if (tgl.hasClass("is-open") || tgl.hasClass("is-opened")) {
+                    tgl.removeClass("is-open").removeClass("is-opened");
+                    tgl.addClass("is-close").attr({ "aria-expanded": "false", "aria-hidden": "true" });
+                    headingHeight = tgl.find('.acd-panel-heading').outerHeight(true);
+                    tgl.height(headingHeight);
+                } else {
+                    tgl.removeClass("is-close").removeClass("is-closed");
+                    tgl.addClass("is-open").attr({ "aria-expanded": "true", "aria-hidden": "false" });
+                    headingHeight = tgl.find('.acd-panel-heading').outerHeight(true);
+                    contentHeight = tgl.find('.acd-panel-content').outerHeight(true);
+                    tgl.height(headingHeight + contentHeight + "px");
+                }
+                return false;
+            }
+        });
+
+        $(function () {
+            var urlHash = location.hash;
+
+            if (urlHash) {
+                var titleHash = $(".acd-panel").find(urlHash);
+                var contentHash = $(".acd-panel-content").find(urlHash);
+
+                if (titleHash.hasClass("acd-panel-heading")) {
+                    var tgl = $(titleHash).parents(".acd-panel");
+                    tgl.removeClass("is-close").removeClass("is-closed");
+                    tgl.addClass("is-opened").attr({ "aria-expanded": "true", "aria-hidden": "false" });
+                    headingHeight = tgl.find('.acd-panel-heading').outerHeight(true);
+                    contentHeight = tgl.find('.acd-panel-content').outerHeight(true);
+                    tgl.height(headingHeight + contentHeight + "px");
+                }
+                if (contentHash.length) {
+                    var tgl = $(contentHash).parents(".acd-panel");
+                    tgl.removeClass("is-close").removeClass("is-closed");
+                    tgl.addClass("is-opened").attr({ "aria-expanded": "true", "aria-hidden": "false" });
+                    headingHeight = tgl.find('.acd-panel-heading').outerHeight(true);
+                    contentHeight = tgl.find('.acd-panel-content').outerHeight(true);
+                    tgl.height(headingHeight + contentHeight + "px");
+                }
+            }
+        });
+        $('a[href^="#"]:not(.btn-tab):not(#page-top a)').click(function () {
+            var href = $(this).attr("href");
+            if (href) {
+                var titleHash = $(".acd-panel").find(href);
+                var contentHash = $(".acd-panel-content").find(href);
+
+                if (titleHash.hasClass("acd-panel-heading")) {
+                    var tgl = $(titleHash).parents(".acd-panel");
+                    tgl.removeClass("is-close").removeClass("is-closed");
+                    tgl.addClass("is-open").attr({ "aria-expanded": "true", "aria-hidden": "false" });
+                    headingHeight = tgl.find('.acd-panel-heading').outerHeight(true);
+                    contentHeight = tgl.find('.acd-panel-content').outerHeight(true);
+                    tgl.height(headingHeight + contentHeight + "px");
+                }
+                if (contentHash.length) {
+                    var tgl = $(contentHash).parents(".acd-panel");
+                    tgl.removeClass("is-close").removeClass("is-closed");
+                    tgl.addClass("is-open").attr({ "aria-expanded": "true", "aria-hidden": "false" });
+                    headingHeight = tgl.find('.acd-panel-heading').outerHeight(true);
+                    contentHeight = tgl.find('.acd-panel-content').outerHeight(true);
+                    tgl.height(headingHeight + contentHeight + "px");
+                }
+            }
+            return false;
+        });
+        if ($(".acd-01 .btn-acd").length) {
+            $(".acd-01 .btn-acd button").on("click", function () {
+                if ($(this).hasClass("btn-open")) {
+                    $(this).removeClass("btn-open");
+                    $(this).addClass("btn-close");
+                    $(this).html("すべての項目を閉じる")
+                    var acd = $(this).parents(".acd-01");
+                    var tgl = acd.find(".acd-panel");
+                    tgl.removeClass("is-close").removeClass("is-closed");
+                    tgl.addClass("is-open").attr({ "aria-expanded": "true", "aria-hidden": "false" });
+
+                    (function () {
+                        var all = tgl;
+                        var i = 0;
+                        var max = all.length;
+
+                        for (i; i < max; i++) {
+                            var headingHeight = ($(all[i])).find('.acd-panel-heading').outerHeight(true);
+                            var contentHeight = ($(all[i])).find('.acd-panel-content').outerHeight(true);
+                            ($(all[i])).height(headingHeight + contentHeight + "px");
+                        };
+                    }());
+                } else {
+                    $(this).removeClass("btn-close");
+                    $(this).addClass("btn-open");
+                    $(this).html("すべての項目を開く")
+                    var acd = $(this).parents(".acd-01");
+                    var tgl = acd.find(".acd-panel");
+                    tgl.removeClass("is-open").removeClass("is-opened");
+                    tgl.addClass("is-close").attr({ "aria-expanded": "false", "aria-hidden": "true" });
+
+                    (function () {
+                        var all = tgl;
+                        var i = 0;
+                        var max = all.length;
+
+                        for (i; i < max; i++) {
+                            var headingHeight = ($(all[i])).find('.acd-panel-heading').outerHeight(true);
+                            ($(all[i])).height(headingHeight);
+                        };
+                    }());
+                }
+                return false;
+            });
+        }
+    }
 });
